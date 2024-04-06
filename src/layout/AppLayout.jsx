8287ff13b64dff63a0a1aaef684c439ed308a3cb
Hwiho Lee <lhh323@gmail.com>
2024-04-06 21:29:11 +0900

    4/6

---
+++ src/layout/AppLayout.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+const AppLayout = () => {
+  return (
+    <div>
+      <Navbar expand="lg" className="nav" variant='dark'>
+        <Container fluid>
+          <Navbar.Brand href="#">
+            <img width ={70} src='https://images.ctfassets.net/4cd45et68cgf/4nBnsuPq03diC5eHXnQYx/d48a4664cdc48b6065b0be2d0c7bc388/Netflix-Logo.jpg'/></Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/movies">Movies</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-danger">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet/>
+    </div>
+  )
+}
+
+export default AppLayout
---
+++ src/pages/MovieDetail/MovieDetailPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MovieDetailPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MovieDetailPage
